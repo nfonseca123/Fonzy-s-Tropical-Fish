85f58f04be1e823a8bf9346aca76d001b00a8d7c
Noah Fonseca <nfonseca@rrc.ca>
2025-06-29 18:52:15 -0500

    Updated products show and index page with boostrap grid

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,4 +58,5 @@ end
   )
 end
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# Create an admin user
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
