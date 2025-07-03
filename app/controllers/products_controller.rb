cd9a010af288b2eb3c488f26c87492af26d34765
Noah Fonseca <nfonseca@rrc.ca>
2025-07-03 09:54:47 -0500

    Addded products filtering

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,15 @@
 class ProductsController < ApplicationController
-  def index
-    @products = Product.all
+def index
+  @products = Product.all
+  @categories = Category.all
+  @query = params[:query]
+  @products = @products.where("products.name LIKE :q OR products.description LIKE :q", q: "%#{@query}%")
+
+  @selected_category = params[:category_id]
+  if @selected_category.present?
+    @products = @products.joins(:categories).where(categories: { id: @selected_category })
   end
+end
 
   def show
     @product = Product.find(params[:id])
