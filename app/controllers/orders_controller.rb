6ac4332d3590747e897a3c661cf0d7b04db975a6
Noah Fonseca <nfonseca@rrc.ca>
2025-07-07 19:39:29 -0500

    Added Tax calculator at checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,32 @@
 class OrdersController < ApplicationController
   def checkout
-    @cart_items = Product.find(cart.keys)
-    @cart_totals = @cart_items.each_with_object({}) do |product, hash|
-      quantity = cart[product.id.to_s]
-      hash[product.id] = product.current_price * quantity
+    customer = current_customer
+    province = customer&.province
+
+    unless province
+      redirect_to root_path, alert: "We couldn't determine your province. Please update your profile." and return
     end
+    @province_name = province.name
+    @gst_rate = province.gst_rate.to_f / 100
+    @pst_rate = province.pst_rate.to_f / 100
+    @hst_rate = province.hst_rate.to_f / 100
+
+
+    tax_multiplier = 1 + @gst_rate + @pst_rate + @hst_rate
+
+    @cart_items = Product.find(cart.keys)
+
+    @cart_totals = @cart_items.each_with_object({}) do |product, hash|
+      quantity = cart[product.id.to_s].to_i
+      price_with_tax = product.current_price * quantity * tax_multiplier
+      hash[product.id] = price_with_tax
+    end
+
     @cart_total = @cart_totals.values.sum
     @order = Order.new
   end
+
+
 
   def start_payment
   session = Stripe::Checkout::Session.create(
